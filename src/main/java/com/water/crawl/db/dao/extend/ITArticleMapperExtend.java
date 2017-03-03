8cb51a3961386b30a4777350fa74c79bdb3fae08
zhangmiaojie <136218949@qq.com>
2017-03-03 17:27:56 +0800

    项目整合dubbo，使用zookeeper注册服务中心，暴露es接口。

--- src/main/java/com/water/crawl/db/dao/extend/ITArticleMapperExtend.java
+++
@@ -1,4 +0,0 @@
-package com.water.crawl.db.dao.extend;
-
-public interface ITArticleMapperExtend {
-}
--- src/main/java/com/water/crawl/utils/QueueClientHelper.java
+++
@@ -1,8 +0,0 @@
-package com.water.crawl.utils;
-
-/**
- * Created by zhangmiaojie on 2016/12/2.
- */
-public class QueueClientHelper {
-    public static String QUEUE_FETCH_IMG_URL = "queue_fetch_img_url";
-}
