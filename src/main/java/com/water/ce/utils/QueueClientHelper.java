d5ed1f9de04498d0a4d51bfa10167d263e3dcab7
Zhang miaojie <136218949@qq.com>
2017-11-17 20:07:28 +0800

    爬虫框架重构

--- src/main/java/com/water/ce/utils/QueueClientHelper.java
+++ src/main/java/com/water/ce/utils/QueueClientHelper.java
@@ -5,8 +5,8 @@ package com.water.ce.utils;
  */
 public class QueueClientHelper {
 
-//    public static String QUEUEHOST = "queue-server-01:6379|queue-server-02:6379";
-    public static String QUEUEHOST = "119.23.71.245:6379|118.31.33.195:6379";
+    public static String QUEUEHOST = "127.0.0.1:6379|127.0.0.1:6379";
+//    public static String QUEUEHOST = "119.23.71.245:6379|118.31.33.195:6379";
 
     public static final String FETCH_ARTICLE_QUEUE = "fetch_article_queue";
 }
